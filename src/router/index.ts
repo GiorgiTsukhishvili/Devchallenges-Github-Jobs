1d126f592e86e17335989a743ac251aedc5bd5a8
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2023-05-16 20:08:40 +0400

    feat: add jobs view and route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { JobsView } from "@/views";
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
-})
+  routes: [{ path: "/", name: "jobs", component: JobsView }],
+});
 
-export default router
+export default router;
